d58f3ec0a22dbeb3dfdb30d1e09d6c05e3612fa9
Austin <aus343@gmail.com>
2021-03-17 13:49:12 -0500

    Added Project Files

---
+++ PolytopiaExplorer/PolytopiaPatch.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityExplorer.Helpers;
+
+namespace PolytopiaExplorer
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("Polytopia.exe")]
+    public class PolytopiaPatch : BasePlugin
+    {
+        public const string NAME = "PolytopiaExplorer";
+        public const string VERSION = "1.0.0";
+        public const string AUTHOR = "DigiWorm";
+        public const string GUID = "com.digiworm.polytopiaexplorer";
+
+        public Harmony Harmony { get; } = new Harmony(GUID);
+        public static ManualLogSource logger = new ManualLogSource("PolytopiaExplorer");
+
+        public override void Load()
+        {
+            BepInEx.Logging.Logger.Sources.Add(logger);
+            logger.Log(LogLevel.Info, "Loaded PolytopiaExplorer v1.0");
+            Harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ClientInteraction), nameof(ClientInteraction.UpdateMouseInput))]
+        public static class UIPatch
+        {
+            public static bool Prefix()
+            {
+                if (UnityExplorer.UI.UIManager.ShowMenu)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(CameraController), nameof(CameraController.UpdateInput))]
+        public static class CamPatch
+        {
+            public static bool Prefix()
+            {
+                if (UnityExplorer.UI.UIManager.ShowMenu)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PolytopiaExplorer
-Patch to use Unity Explorer in the game Polytopia
+# Polytopia
+## Unity Explorer Patch
+This is a patch for the game [Polytopia](https://store.steampowered.com/app/874390/The_Battle_of_Polytopia/) in order to run [Unity Explorer](https://github.com/sinai-dev/UnityExplorer/). Requires [BepInEx](https://github.com/BepInEx/BepInEx/) and [Unity Explorer](https://github.com/sinai-dev/UnityExplorer/).
+## Instructions
+Drop the DLL in the Releases tab into BepInEx's plugins folder
